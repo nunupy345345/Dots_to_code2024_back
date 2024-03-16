fb24a3ffc511d3bbd5d0acce371f068935178289
nac-39 <83295760+nac-39@users.noreply.github.com>
2024-03-16 22:39:12 +0900

    リファクタリング

--- app/main.py
+++ app/main.py
@@ -1,13 +1,16 @@
 import uvicorn
 from fastapi import FastAPI
+
 # MEMO ↓CORSを有効にしたいとき使う
 # from fastapi_cors import CORS 
 
 app = FastAPI()
 
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
