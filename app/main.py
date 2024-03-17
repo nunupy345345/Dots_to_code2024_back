994eddde60895dafe64821890842e82871b4d583
nac-39 <83295760+nac-39@users.noreply.github.com>
2024-03-17 23:19:04 +0900

    refactor: format

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 import uvicorn
-from fastapi import FastAPI,HTTPException
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
 
 # MEMO ↓CORSを有効にしたいとき使う
@@ -15,6 +15,7 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+
 @app.post("/category")
 async def get_items_by_category(request_body: CategoryModel):
     try:
@@ -22,13 +23,14 @@ async def get_items_by_category(request_body: CategoryModel):
         category_list = request_body.category_list
 
         # todo: ユーザーが選んだカテゴリを保存しておく(useridとcategoryidを紐づけて保存)
-        
+
         response = {"response": "Successful Operation"}
-        
+
     except Exception as e:
-            # COMMENT: エラーが発生した場合はHTTP例外を発生させる
-            raise HTTPException(status_code=500, detail=f"Error updating documents: {str(e)}")
+        # COMMENT: エラーが発生した場合はHTTP例外を発生させる
+        raise HTTPException(status_code=500, detail=f"Error updating documents: {str(e)}")
     return JSONResponse(status_code=200, content=response)
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
--- app/models.py
+++ app/models.py
@@ -1,9 +1,8 @@
 from pydantic import BaseModel
 
+
 # todo: モデルをつくる(多分変わる)
 
 class CategoryModel(BaseModel):
     user_id: int
     category_list: list
-    
-    
