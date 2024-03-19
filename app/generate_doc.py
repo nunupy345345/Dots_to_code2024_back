0de58d093bec11f5503aa15132e6a46d035fd0fc
nac-39 <83295760+nac-39@users.noreply.github.com>
2024-03-19 11:46:23 +0900

    コミット時にapi docを生成するようにした

--- app/generate_doc.py
+++ app/generate_doc.py
@@ -32,5 +32,5 @@ HTML_TEMPLATE = """<!DOCTYPE html>
 """
 
 if __name__ == "__main__":
-    with open(f"docs/api.html", "w") as fd:
+    with open(f"../docs/api.html", "w") as fd:
         print(HTML_TEMPLATE % json.dumps(app.openapi()), file=fd)
