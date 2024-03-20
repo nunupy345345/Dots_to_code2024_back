d327bb2e36a5987181a13d62d0050d4dd32fd740
nunupy345345 <nunupy345345@airi9ri>
2024-03-20 18:46:15 +0900

    /items/{user_id} 完成

--- app/handler/item.py
+++ app/handler/item.py
@@ -1,4 +1,4 @@
-from .schema import ItemResponseModel
+from .schema import ItemResponseModel,ItemModel
 from services.item import ItemService
 from services.user import UserService
 from fastapi import HTTPException
@@ -13,12 +13,21 @@ def send_items_handler(userId: str):
         /items/{userId}で使用
         そのユーザーに好き嫌いを判定してもらうアイテムを6つ返す
         """
-        # items = []
         us = UserService()
         items = us.get_preference_unregistered_items(userId)
         selected_items = random.sample(items, 6)
-        # response = UserRepository.find_by_id(userId)
-        response = items
+        item_models = [ItemModel(
+            id=item.id,
+            name=item.name,
+            category=item.category,
+            price=item.price,
+            url=item.url,
+            image_url=item.image_url,
+            evaluations=item.evaluations
+        ) for item in selected_items]
+
+        # ItemResponseModelを作成
+        response = ItemResponseModel(ItemList=item_models)
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error create user: {str(e)}")
     return JSONResponse(status_code=200, content=response.model_dump_json())
