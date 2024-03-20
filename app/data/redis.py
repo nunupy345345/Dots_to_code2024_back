b510895bb1e870bafa166d196c4a7d53bbf7507d
nac-39 <83295760+nac-39@users.noreply.github.com>
2024-03-20 10:57:05 +0900

    fix: 本番環境のredisの認証情報をenvから読み込むように変更

--- app/data/redis.py
+++ app/data/redis.py
@@ -5,4 +5,10 @@ import redis
 
 class RedisClient:
     def __init__(self):
-        self.redis = redis.StrictRedis(host=os.environ.get("REDIS_HOST"), port=6379, db=0)
+        if os.environ.get("ENV") == "production":
+            self.redis = redis.StrictRedis(host=os.environ.get("REDIS_HOST"), port=int(os.environ.get("REDIS_PORT")),
+                                           password=os.environ.get("REDIS_PASSWORD"),
+                                           ssl=True,
+                                           db=0)
+        else:
+            self.redis = redis.StrictRedis(host=os.environ.get("REDIS_HOST"), port=6379, db=0)
