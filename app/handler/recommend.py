7768832670ec40a4a07c193fae4680d96f181175
nunupy345345 <nunupy345345@airi9ri>
2024-03-22 09:56:45 +0900

    pull前commit

--- app/handler/recommend.py
+++ app/handler/recommend.py
@@ -1,14 +1,20 @@
 from .schema import RecommendResponseModel
 from fastapi import HTTPException
 from fastapi.responses import JSONResponse
-
+from services.user import UserService
 
 def recommend_handler(user_id: str):
     try:
         """
         response: RecommendResponseModel
+        そのユーザーにおすすめする商品5つを返す
+        評価が高いもの順
         """
-        response = {"response": "Successful Operation"}
+        us = UserService()
+        recommended_items = us.get_updated_user_recommend_items(user_id)
+        response = recommended_items
+        response = RecommendResponseModel(item_list=recommended_items)
+        # response = {"status":"Successful Operation"}
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error recommend items: {str(e)}")
-    return JSONResponse(status_code=200, content=response)
+    return JSONResponse(status_code=200, content=response.model_dump_json())
--- app/handler/schema/recommend.py
+++ app/handler/schema/recommend.py
@@ -1,5 +1,13 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field,HttpUrl
 
-
+class ItemModel(BaseModel):
+    id: int
+    name: str
+    category: str
+    price: int
+    url: HttpUrl
+    image_url: HttpUrl
+    evaluations: list[int]
+    
 class RecommendResponseModel(BaseModel):
-    recommend_list : list[str] = Field(..., min_items=5, max_items=5)
+    recommend_list : list[ItemModel] = Field(..., min_items=5, max_items=5)
